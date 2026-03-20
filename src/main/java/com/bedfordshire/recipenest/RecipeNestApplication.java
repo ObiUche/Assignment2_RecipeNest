91549e55b0d5244fb1cec0dad693edc9c3ed77d2
Obinna Uche <75355071+ObiUche@users.noreply.github.com>
2026-03-20 17:44:48 +0000

    Initialised complete

---
+++ src/main/java/com/bedfordshire/recipenest/RecipeNestApplication.java
@@ -0,0 +1,13 @@
+package com.bedfordshire.recipenest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeNestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bedfordshire/recipenest/RecipeNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bedfordshire.recipenest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
